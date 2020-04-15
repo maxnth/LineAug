bb1e7c2aa49e396a8cbdf7f09d47ea1ca937b5c1
maxnth <maximilian.noeth@protonmail.com>
2020-04-15 12:05:48 +0200

    add docstrings

--- src/Augmentation.py
+++ src/Augmentation.py
@@ -5,13 +5,29 @@ from PIL import Image
 
 
 class Augmentation:
-    def __init__(self, img_data: np.array, mean_bg: False):
+    """Represents a single augmentation of a line image.
+
+    Args:
+        img_data (np.ndarray): Numpy array representation of an image.
+        bg (bool): Whether to add background noise during the augmentation.
+
+    Attributes:
+        img_data (np.ndarray): Numpy array representation of an image.
+        bg (bool): Whether to add background noise during the augmentation.
+        aug_data (np.ndarray): Numpy array representation of the augmented image. Basis for augmentation calculations.
+    """
+    def __init__(self, img_data: np.ndarray, bg: False):
         self.img_data = img_data.T
-        self.mean_bg = mean_bg
+        self.bg = bg
 
         self.aug_data = self.augment_line()
 
     def augment_line(self) -> np.ndarray:
+        """Calculate augmented line image based on the original image data.
+
+        Returns:
+            np.ndarray: Numpy array representation of the augmented image.
+        """
         original_dtype = self.img_data.dtype
         data = self.img_data.astype(np.float)
         m = data.max()
@@ -22,12 +38,17 @@ class Augmentation:
             noise = ocrodeg.bounded_gaussian_noise(data.shape, sigma, 3.0)
             data = ocrodeg.distort_with_noise(data, noise)
 
-        if self.mean_bg:
+        if self.bg:
             data = ocrodeg.printlike_multiscale(data, blur=1, inverted=True)
         data = (data * 255 / data.max()).astype(original_dtype)
         return data.T
 
     def export(self, out_filename: str):
+        """Exports the augmented image to image file.
+
+        Args:
+            out_filename (str): Output filename for the generated line image.
+        """
         formatted = (255 - self.aug_data * 255 / np.max(255 - self.aug_data)).astype('uint8')
         img = Image.fromarray(formatted).convert("RGB")
         img.save(out_filename)
